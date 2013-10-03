39b831bad139fd4b2b8854e836bb9c5a63d04554
Max Lincoln <max@devopsy.com>
2013-10-03 12:43:27 -0300

    schema generator working

--- lib/json/schema_generator.rb
+++ lib/json/schema_generator.rb
@@ -1,69 +1,96 @@
+require 'stringio'
+require 'json'
 module JSON
   class SchemaGenerator
-    class DataWalker
-      def initialize file_name
-        @buffer = StringIO.new
-        @buffer.puts "{"
-        @buffer.puts '"$schema": "http://json-schema.org/draft-03/schema#",'
-        @buffer.print '"description": "Generated from '
-        @buffer.print file_name
-        @buffer.puts '",'
-      end
-
-      def close
-        @buffer.puts "}"
-      end
-
-      def walk_data data, buffer = @buffer
-        case data
-        when String
-          buffer.puts '"type": "string",'
-          buffer.puts '"required": true'
-        when Integer
-          buffer.puts '"type": "integer",'
-          buffer.puts '"required": true'
-        when Float
-          buffer.puts '"type": "number",'
-          buffer.puts '"required": true'
-        when Array
-          buffer.puts buffer.puts '"type": "array",'
-          buffer.puts buffer.puts '"items": {'
-          buffer.puts walk_data data.first, StringIO.new
-          buffer.puts '},'
-          buffer.puts '"minItems": 1,'
-          buffer.puts '"uniqueItems": true'
-          buffer.puts "}"
-        when Hash
-          buffer.puts '"type": "object",'
-          buffer.print '"properties": {'
-          subsection = data.map do |k,v|
-            subbuffer = StringIO.new
-            subbuffer.puts
-            subbuffer.puts "\"#{k}\": {"
-            walk_data v, subbuffer
-            subbuffer.string
-          end.join "},"
-          buffer << subsection
-          buffer.puts "}"
-        else
-          raise "Unknown type! #{data.class}"
-        end
-        buffer.string
-      end
-
-      def result
-        @buffer.string
+    class << self
+      def generate name, data
+        generator = SchemaGenerator.new name
+        generator.generate data
       end
     end
 
-    class << self
-      def generate file
-        dw = DataWalker.new file
-        data = JSON.load(File.read file)
-        dw.walk_data data
-        dw.close
-        dw.result
+    def initialize name
+      @buffer = StringIO.new
+      @name = name
+    end
+
+    def generate raw_data
+      # Write header
+      @buffer.puts "{"
+      @buffer.puts '"$schema": "http://json-schema.org/draft-03/schema#",'
+      @buffer.print '"description": "Generated from '
+      @buffer.print @name
+      @buffer.puts '",'
+
+      # 
+      data = JSON.load(raw_data)
+
+      @buffer.puts create_hash(data)
+      close
+      result
+    end
+
+    protected
+
+    def close
+      @buffer.puts "}"
+    end
+
+    def create_values(key, value)
+      buffer = StringIO.new
+      buffer.puts "\"#{key}\": {"
+      case value
+      when TrueClass, FalseClass
+        buffer.puts '"type": "boolean",'
+        buffer.puts '"required": true'
+
+      when String
+        buffer.puts '"type": "string",'
+        buffer.puts '"required": true'
+
+      when Integer
+        buffer.puts '"type": "integer",'
+        buffer.puts '"required": true'
+
+      when Float
+        buffer.puts '"type": "number",'
+        buffer.puts '"required": true'
+      when Array
+        buffer << create_array(value)
+      when Hash
+        buffer << create_hash(value)
+      else
+        raise "Unknown Type for #{key}! #{value.class}"
       end
+      buffer.print "}"
+      buffer.string
+    end
+
+    def create_hash(data)
+      buffer = StringIO.new
+      buffer.puts '"type": "object",'
+      buffer.puts '"properties": {'
+      items = data.collect do |k,v|
+        create_values k,v
+      end
+      buffer << items.join(",\n")
+      buffer.puts '}'
+
+      buffer.string
+    end
+
+    def create_array(data)
+      buffer = StringIO.new
+      buffer.puts '"type": "array",'
+      buffer.puts '"minItems": 1,'
+      buffer.puts '"uniqueItems": true,'
+      buffer.puts create_values("items", data.first)
+
+      buffer.string
+    end
+
+    def result
+      @buffer.string
     end
   end
 end
--- spec/json/schema_generator_spec.rb
+++ spec/json/schema_generator_spec.rb
@@ -1,10 +1,28 @@
 # encoding: utf-8
 require 'json/schema_generator'
+require 'ansi/code'
+require 'ansi/diff'
+require 'tempfile'
+
+def clean_output(output)
+  output = ANSI.unansi(output)
+  output.gsub!(/^\s+/,'')
+  output.gsub!(/\s+$/,'')
+  output.gsub!("\e[0G", '')
+  output
+
+  file = Tempfile.new('json')
+  file.write output
+  file.flush
+  `cat #{file.path} | python -m json.tool`
+end
 
 def compare_without_whitespace actual, expected
-  actual_cleaned = actual.gsub(/^\s+/, '').gsub(/\s+$/, '')
-  expected_cleaned = expected.gsub(/^\s+/, '').gsub(/\s+$/, '')
+  actual_cleaned = clean_output actual
+  expected_cleaned = clean_output expected
   expect(actual_cleaned).to eq expected_cleaned
+  # require 'pry'; binding.pry
+  # raise ANSI::Diff.new(actual_cleaned, expected_cleaned)
 end
 
 module JSON
@@ -14,7 +32,8 @@ module JSON
         example = File.basename example_file
         expected = File.expand_path(example, 'spec/fixtures/schemas')
         it "generates the expected schema for #{example}" do
-          schema = described_class.generate example_file
+          data = File.read example_file
+          schema = described_class.generate example_file, data
           compare_without_whitespace schema, File.read(expected)
         end
       end
